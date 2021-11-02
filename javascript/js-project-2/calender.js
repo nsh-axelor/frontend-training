87c54de917df129246a2110f8cd31adf1808c663
nsh-axelor <nandit.axelor@gmail.com>
2021-11-02 18:45:37 +0530

    Added styles to project-4 and added extra feature's UI to calender

--- javascript/js-project-2/calender.js
+++ javascript/js-project-2/calender.js
@@ -1,16 +1,21 @@
-var today = new Date() 
+// Getting todays date
+var today = new Date()
+
+// Getting given months day-1 date
 var date = new Date(today.getFullYear(),today.getMonth());
 
+// How Many days are there in that perticular month
 var daysInMonth = 32 - new Date(today.getFullYear(), today.getMonth(), 32).getDate();
-console.log(daysInMonth);
-console.log(date.getDay());
-console.log(date.getDate());
 
 
 const calender = document.getElementById("calender")
-console.log(calender.children);
-var j = 0;
-var row = 0
+
+
+
+
+let row = 0
+let k = 1
+
 
 if(date.getDay() != 0){
     for(var i =0;i<date.getDay();i++){
@@ -19,24 +24,25 @@ if(date.getDay() != 0){
     }
 }
 
-var k = 1
+
+
 while(k <= daysInMonth){
     for(var i = 0;i<=6;i++){
         if (calender.children[row].childElementCount >=7 ){
             row ++;
             break
         }
+        
         const td = document.createElement("td");
         const textNode = document.createTextNode(k)
         k++
         td.appendChild(textNode)
-
         calender.children[row].appendChild(td)
-        if(k >= 31){
+
+        if(k >= daysInMonth){
             break
         }
     }
 }
 
 
-console.log(calender.children);

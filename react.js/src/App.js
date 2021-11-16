a01cb35273bf195bf277492a90ce77ad283362ac
nsh-axelor <nandit.axelor@gmail.com>
2021-11-16 18:56:48 +0530

    React Project Created and complted till components and props

---
+++ react.js/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ReactPractice from './ReactPractice.jsx'
+function App() {
+  return (
+    <div className="App">
+
+        {/* UnComment ReactPractice Tag for reviewing React Practice file */}
+        <ReactPractice />   
+    </div>
+  );
+}
+
+export default App;

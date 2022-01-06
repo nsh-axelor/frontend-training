c84497a85cbe76b6a1429d6ed59f84e1a302858c
nsh-axelor <nandit.axelor@gmail.com>
2022-01-06 12:54:58 +0530

    TImeInput Component Alignment Changes solved

--- react.js/src/Calendar-Project/TimeInput/TimeInput.js
+++ react.js/src/Calendar-Project/TimeInput/TimeInput.js
@@ -1,49 +1,59 @@
-import React from 'react';
-import "./TimeInput.css"
-
+import React from "react";
+import "./TimeInput.css";
 
 // Months objects
 const months = {
-    0: "January",
-    1: "February",
-    2: "March",
-    3: "April",
-    4: "May",
-    5: "June",
-    6: "July",
-    7: "August",
-    8: "September",
-    9: "October",
-    10: "November",
-    11: "December"
-}
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December",
+};
 
 const TimeInput = ({ month, year, setMonth, setYear }) => {
+  // Handling Month Change
+  const handleMonthChage = (e) => {
+    setMonth(Number(e.target.value));
+  };
 
-    // Handling Month Change
-    const handleMonthChage = (e) => {
-        setMonth(Number(e.target.value))
-    }
+  // Handling Year Change
+  const handleYearChange = (e) => {
+    setYear(Number(e.target.value));
+  };
 
-    // Handling Year Change
-    const handleYearChange = (e) => {
-        setYear(Number(e.target.value))
-    }
-
-
-
-    return (
-        <div className='timeInput'>
-            <select value={month} className="input" onChange={handleMonthChage} name="" id="month_select">
-                {
-                    Object.keys(months).map((key) => (
-                        <option value={key} key={key}>{months[key]}</option>
-                    ))
-                }
-            </select>
-            <input className="input" onChange={handleYearChange} type="number" value={year} name="year" id="year_input" required></input>
-        </div>
-    )
-}
+  return (
+    <div className="timeInput">
+      <select
+        value={month}
+        className="input"
+        onChange={handleMonthChage}
+        name=""
+        id="month_select"
+      >
+        {Object.keys(months).map((key) => (
+          <option value={key} key={key}>
+            {months[key]}
+          </option>
+        ))}
+      </select>
+      <input
+        className="input"
+        onChange={handleYearChange}
+        type="number"
+        value={year}
+        name="year"
+        id="year_input"
+        required
+      ></input>
+    </div>
+  );
+};
 
 export default TimeInput;
--- react.js/src/Calendar-Project/View/View.js
+++ react.js/src/Calendar-Project/View/View.js
@@ -1,62 +1,56 @@
-import React from 'react';
-import "./View.css"
+import React from "react";
+import "./View.css";
 
 // Array of weekdays
-const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const CalendarComponent = ({ month, year }) => {
+  // This function will complete calendar
+  const createCalendar = (month, year) => {
+    let day = 1;
+    const calendar_tr = [];
+    let calendar_td = [];
+    const date = new Date(year, month);
+    const daysInMonth = 32 - new Date(year, month, 32).getDate();
 
-    // This function will complete calendar
-    const createCalendar = (month, year) => {
-
-        let day = 1
-        const calendar_tr = []
-        let calendar_td = []
-        const date = new Date(year, month);
-        const daysInMonth = 32 - new Date(year, month, 32).getDate();
-        
-        // Filling empty spaces whenever month does not start with sunday
-        if (date.getDay() !== 0) {
-            for (let i = 0; i < date.getDay(); i++) {
-                calendar_td.push(<td empty = "true" key={"empty"+i}></td>)
-            }
-        }
-
-        // Filling dates with respect to their weekday
-        while (day <= daysInMonth) {
-            if (calendar_td.length === 7) {
-                calendar_tr.push(<tr key = {month+day+year}>{calendar_td}</tr>)
-                calendar_td = []
-            }
-            calendar_td.push(<td key = {month+day+year}>{day}</td>)
-            if (day === daysInMonth) {
-                calendar_tr.push(<tr key = {day}>{calendar_td}</tr>)
-            }
-            day++
-        }
-        return calendar_tr
+    // Filling empty spaces whenever month does not start with sunday
+    if (date.getDay() !== 0) {
+      for (let i = 0; i < date.getDay(); i++) {
+        calendar_td.push(<td empty="true" key={"empty" + i}></td>);
+      }
     }
 
+    // Filling dates with respect to their weekday
+    while (day <= daysInMonth) {
+      if (calendar_td.length === 7) {
+        calendar_tr.push(<tr key={month + day + year}>{calendar_td}</tr>);
+        calendar_td = [];
+      }
+      calendar_td.push(<td key={month + day + year}>{day}</td>);
+      if (day === daysInMonth) {
+        calendar_tr.push(<tr key={day}>{calendar_td}</tr>);
+      }
+      day++;
+    }
+    return calendar_tr;
+  };
 
-
-    return (
-        <div className='calendarComponent'>
-            <table>
-                <thead>
-                    <tr>
-                        {
-                            days.map((day) => (
-                                (<th key={day}>{day}</th>)
-                            ))
-                        }
-                    </tr>
-                </thead>
-                <tbody>
-                    {(year >= 1970) && (year <= 2050) && (createCalendar(month, year))}
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  return (
+    <div className="calendarComponent">
+      <table>
+        <thead>
+          <tr>
+            {days.map((day) => (
+              <th key={day}>{day}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {year >= 1970 && year <= 2050 && createCalendar(month, year)}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default CalendarComponent;
